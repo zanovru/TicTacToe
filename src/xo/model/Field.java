e34b319dde6bf7a7c2c0c015a376677f4b54bcde
Vitaly Ruzanov <vp.ruzanov@gmail.com>
2019-10-26 22:35:27 +0300

    Added exceptions for methods getFigure and SetFigure

--- src/xo/model/Field.java
+++ src/xo/model/Field.java
@@ -1,21 +1,44 @@
 package xo.model;
 
+import xo.model.exceptions.AlreadyOccupiedException;
+import xo.model.exceptions.InvalidPointException;
+
 import java.awt.*;
 
 public class Field {
 
     private static final int FIELD_SIZE = 3;
+    private static final int MIN_COORDINATE = 0;
+    private static final int MAX_COORDINATE = FIELD_SIZE;
     Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
 
     public static int getFieldSize() {
         return FIELD_SIZE;
     }
 
-    public Figure getFigure(final Point point) {
+    public Figure getFigure(final Point point) throws InvalidPointException {
+        if(!checkPoint(point)) {
+            throw new InvalidPointException();
+        }
         return field[point.x][point.y];
     }
 
-    public void setFigure(final Point point, final Figure figure) {
+    public void setFigure(final Point point, final Figure figure) throws InvalidPointException,
+                                                                            AlreadyOccupiedException {
+        if(!checkPoint(point)) {
+            throw new InvalidPointException();
+        }
+        if(field[point.x][point.y] != null) {
+            throw new AlreadyOccupiedException();
+        }
         field[point.x][point.y] = figure;
+    }
+
+    public boolean checkPoint(Point point) {
+        return checkCoordinate(point.x) && checkCoordinate(point.x);
+    }
+
+    public boolean checkCoordinate(final int coordinate) {
+        return coordinate >= MIN_COORDINATE && coordinate < MAX_COORDINATE;
     }
 }
